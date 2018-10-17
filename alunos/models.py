c6ce0d4caaf6e67edab844683b5150d71b9a82d4
Andre Fonseca <aoqfonseca@gmail.com>
2018-10-17 17:55:02 -0300

    Adicionado o primeiro modelo

--- alunos/models.py
+++ alunos/models.py
@@ -3,4 +3,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+ALUNOS_NIVEIS = (
+    ('branco', 'Branco'),
+    ('amarelo', 'Amarelo'),
+    ('laranja', 'Laranja'),
+    ('verde', 'Verde'),
+    ('azul claro', 'Azul Claro'),
+)
+
+class Aluno(models.Model):
+
+    nome = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=20)
+    email=models.CharField(max_length=255,blank=True,null=True)
+    nivel=models.CharField(choices=ALUNOS_NIVEIS)
