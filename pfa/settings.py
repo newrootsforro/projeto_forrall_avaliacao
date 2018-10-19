639f4335188d962565be54be6c96a38f82a9b195
Andre Fonseca <aoqfonseca@gmail.com>
2018-10-19 18:36:12 -0300

    Acertando os settings pois coloquei coisa errada tentando fazer funcionar o heroku

--- pfa/settings.py
+++ pfa/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django-heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'pfa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DATABASE_URL','django.db.backends.sqlite3'),
-        'NAME': os.environ.get('DATABASE_URL', os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
