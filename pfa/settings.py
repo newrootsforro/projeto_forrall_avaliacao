97cebc4d097a671d0948cdb43e21b8aee08307b8
Andre Fonseca <aoqfonseca@gmail.com>
2018-10-19 18:33:48 -0300

    Adicionado as paradas para funcionar no heroku

--- pfa/settings.py
+++ pfa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,8 +78,8 @@ WSGI_APPLICATION = 'pfa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.environ.get('DATABASE_URL','django.db.backends.sqlite3'),
+        'NAME': os.environ.get('DATABASE_URL', os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
