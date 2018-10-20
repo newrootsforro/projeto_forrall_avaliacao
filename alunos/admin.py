aad9d108defa13da4cda9a0f223eb7b2337d1db3
Andre Fonseca <aoqfonseca@gmail.com>
2018-10-20 19:23:15 -0300

    Alterado o campo de datetime para date.

--- alunos/admin.py
+++ alunos/admin.py
@@ -3,4 +3,13 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Aluno, Pagamentos
+
+class PagamentosInline(admin.StackedInline):
+    model = Pagamentos
+
+@admin.register(Aluno)
+class AlunosAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'nivel',)
+    inlines = [PagamentosInline,]
+
--- alunos/models.py
+++ alunos/models.py
@@ -44,6 +44,6 @@ class Pagamentos(models.Model):
 
     mes=models.CharField(choices=MESES, max_length=55)
     valor = models.IntegerField(default=50)
-    data_pagamento = models.DateTimeField(blank=True,null=True)
+    data_pagamento = models.DateField(blank=True,null=True)
     data_modificacao = models.DateTimeField(auto_now=True)
     observacao = models.TextField()
