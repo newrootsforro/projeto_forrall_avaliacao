a455997dad85fa468b9a4be858f209f06e8a5a6c
Andre Fonseca <aoqfonseca@gmail.com>
2018-10-23 12:57:33 -0300

    Adicionado os modelos ao admin

--- alunos/admin.py
+++ alunos/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Aluno, Pagamentos
+from .models import Aluno, Pagamentos, Nivel
 
 class PagamentosInline(admin.StackedInline):
     model = Pagamentos
@@ -12,4 +12,9 @@ class PagamentosInline(admin.StackedInline):
 class AlunosAdmin(admin.ModelAdmin):
     list_display = ('nome', 'nivel',)
     inlines = [PagamentosInline,]
+
+
+@admin.register(Nivel)
+class NivelAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
 
--- avaliacao/admin.py
+++ avaliacao/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from avaliacao import models
+
+@admin.register(models.Processo)
+class ProcessoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Prova)
+class ProvaAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Criterio)
+class CriterioAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Avaliacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    pass
